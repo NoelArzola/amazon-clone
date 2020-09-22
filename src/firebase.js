0ec1df46d2693b496a4a261d0221c1f1c7381599
Noel Arzola <noel.arzola@gmail.com>
2020-09-21 21:46:17 -0400

    Initial commit, header implemented

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAZCUQzt8_JnWG-SFvqfYK5-V8swKvj7MI",
+  authDomain: "clone-fac52.firebaseapp.com",
+  databaseURL: "https://clone-fac52.firebaseio.com",
+  projectId: "clone-fac52",
+  storageBucket: "clone-fac52.appspot.com",
+  messagingSenderId: "523304396786",
+  appId: "1:523304396786:web:04b00fa8d99dd3197a2d6b",
+  measurementId: "G-FMLTWDWF1P"
+};
