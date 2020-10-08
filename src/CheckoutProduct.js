59c06cbdeaaa5dfc89552d9d0ed02d2bcdc3c280
Noel Arzola <noel.arzola@gmail.com>
2020-10-08 09:43:04 -0400

    Set up for deploy

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./CheckoutProduct.css";
 import StarIcon from "@material-ui/icons/Star";
 import { useStateValue } from "./StateProvider";
-import FlipMove from "react-flip-move";
 import Product from "./Product";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
@@ -18,27 +17,25 @@ function CheckoutProduct({ id, image, title, price, rating }) {
   };
 
   return (
-    <FlipMove>
-      <div className="checkoutProduct" key={Product.id}>
-        <img className="checkoutProduct__image" src={image} alt="" />
+    <div className="checkoutProduct" key={Product.id}>
+      <img className="checkoutProduct__image" src={image} alt="" />
 
-        <div className="checkoutProduct__info">
-          <p className="checkoutProduct__title">{title}</p>
-          <p className="checkoutProduct__price">
-            <small>$</small>
-            <strong>{price}</strong>
-          </p>
-          <div className="checkoutProduct__rating">
-            {Array(rating)
-              .fill()
-              .map((_, i) => (
-                <p>{<StarIcon />}</p>
-              ))}
-          </div>
-          <button onClick={removeFromBasket}>Remove from Cart</button>
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>{<StarIcon />}</p>
+            ))}
         </div>
+        <button onClick={removeFromBasket}>Remove from Cart</button>
       </div>
-    </FlipMove>
+    </div>
   );
 }
 
