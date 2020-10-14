22ba1b241b813fce972e993de127c80fc4b6d732
Noel Arzola <noel.arzola@gmail.com>
2020-10-14 04:05:27 -0400

    Incremental Stripe updates

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")("pk_test_3e0ixkUM0GkeLQ44AxWTXESQ00jsrkAhFw");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +10,18 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// API setup
+
+// App config
+const app = express();
+
+// Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API roots
+app.get("/", (request, response) => res.status(200).send("hello world"));
+
+// Listen commands
+exports.api = functions.https.onRequest(app);
