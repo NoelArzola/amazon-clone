5351bcc08729c3fa6fe66c99a11451df303a43d9
Noel Arzola <noel.arzola@gmail.com>
2020-11-11 17:02:59 -0500

    Deployed version

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-fac52/us-central1/api",
+  baseURL: "https://us-central1-clone-fac52.cloudfunctions.net/api",
+  //"http://localhost:5001/clone-fac52/us-central1/api",
 }); // The API {cloud function} URL
 
 export default instance;
